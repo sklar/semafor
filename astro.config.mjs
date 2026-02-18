479130f40958f484f7fd20c8a42c41613ec74af1
David Sklář <david@sklar.xyz>
2026-02-18 20:01:58 +0100

    feat: add tailwind tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+	vite: { plugins: [tailwindcss()] },
 	integrations: [
 		starlight({
 			title: 'My docs',
